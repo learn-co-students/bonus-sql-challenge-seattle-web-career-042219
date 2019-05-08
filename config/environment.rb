ced9ed2e70af2e00e49c22942ab4f6b1de31b0ce
github username <github email address>
2019-05-08 09:59:48 -0700

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,8 @@ require 'bundler'
 Bundler.require
 
 # Setup a DB connection here
+require 'sqlite3'
+require 'csv'
+require 'pry'
+
+DB = SQLite3::Database.new ":memory:"
--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,21 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require_relative '../config/environment'
+
+rows = DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS guests (
+    id INTEGER PRIMARY KEY,
+    year INTEGER,
+    profession TEXT,
+    show_date TEXT,
+    group_profession TEXT,
+    guest_list TEXT
+  );
+SQL
+
+csv = 'daily_show_guests.csv'
+
+CSV.parse(File.open(csv), headers: true) do |row|
+  DB.execute("INSERT INTO guests(year, profession, show_date, group_profession, guest_list) VALUES ( ?, ?, ?, ?, ? )", row.fields) # equivalent to: [row['name'], row['age']]
+end
+
+DB.execute("SELECT * FROM guests")
